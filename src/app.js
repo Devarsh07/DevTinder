94b1d67cf19487a37843ca95ba9a3cd81e7a1b2f
Devarsh07 <debarshupadhyay7@gmail.com>
2025-04-20 21:27:52 +0530

    playing with routes and handlers

--- src/app.js
+++ src/app.js
@@ -2,14 +2,20 @@ const express = require('express');
 const app = express();
 
 //Handling the request from the port 7777 By request Handlers Middlewares always must remember using of Middleware orders matters;
-app.use("/test",(req,res) => {
-    res.send('Hello from the test page');
+
+app.use("/user",(req,res) => {
+    res.send('Hello from the dashboard page');
 })
-app.use("/users",(req,res) =>{
-    res.send('You are the user bro');
+
+app.get('/user',(req,res) => {
+    res.send("Heelo from user's home page");
 })
-app.use("/",(req,res) => {
-    res.send('Hello from the home page');
+
+app.post('/user',(req,res) => {
+    res.send('User Created Successfully');
+})
+app.delete('/user',(req,res) => {
+    res.send(('User Deleted Successfully'));
 })
 
 
