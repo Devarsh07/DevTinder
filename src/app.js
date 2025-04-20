e71ecfdb764bb265c8020cb1b74dcb479f7da7bf
Devarsh07 <debarshupadhyay7@gmail.com>
2025-04-21 00:08:11 +0530

    Using of Multiple Request HAndlers In a Single Middleware/Routes

--- src/app.js
+++ src/app.js
@@ -3,20 +3,23 @@ const app = express();
 
 //Handling the request from the port 7777 By request Handlers Middlewares always must remember using of Middleware orders matters;
 
-app.use("/user",(req,res) => {
-    res.send('Hello from the dashboard page');
-})
-
-app.get('/user',(req,res) => {
-    res.send("Heelo from user's home page");
-})
-
-app.post('/user',(req,res) => {
-    res.send('User Created Successfully');
-})
-app.delete('/user',(req,res) => {
-    res.send(('User Deleted Successfully'));
-})
+app.use("/user",(req,res,next) => {
+    console.log("Route Handler 1 for user");
+    res.send("Response from 1st Route handler");
+    next();
+    //res.send("Response from 2nd Route handler");
+},
+(req,res,next) =>{
+    console.log(("Route hander 2 for user"));
+    //res.send("Response from 2nd Route handler");
+    next();
+},
+(req,res,next) =>{
+    console.log("Route handler 3 for user");
+    //res.send("Response from 3rd Route handler");
+    next();
+}
+)
 
 
 app.listen(7777,() => {
