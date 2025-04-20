f9211644368f326c0bc4b5e03bbaebb6591e2167
Devarsh07 <debarshupadhyay7@gmail.com>
2025-04-21 02:11:16 +0530

    chaining of middlewares not in single request handlers

--- src/app.js
+++ src/app.js
@@ -3,23 +3,23 @@ const app = express();
 
 //Handling the request from the port 7777 By request Handlers Middlewares always must remember using of Middleware orders matters;
 
-app.use("/user",(req,res,next) => {
-    console.log("Route Handler 1 for user");
-    res.send("Response from 1st Route handler");
+app.use("/user",(req,res,next)=>{//app.use take any request from the route matching /user while it is get or put , patch , delete ok
+    console.log("Entry level 1 and the first changes happen to the request object");
+    next();//this is the most important line in the middleware, if we don't call next() then the request will not go to the next middleware and it will be stuck here.
+})
+app.get("/user",(req,res,next)=>{
+    console.log("Entry level 2 and the second changes happen to the request object");
     next();
-    //res.send("Response from 2nd Route handler");
-},
-(req,res,next) =>{
-    console.log(("Route hander 2 for user"));
-    //res.send("Response from 2nd Route handler");
+})
+app.get("/user",(req,res,next)=>{
+    console.log("Entry level 3 and the third changes happen to the request object");
     next();
-},
-(req,res,next) =>{
-    console.log("Route handler 3 for user");
-    //res.send("Response from 3rd Route handler");
-    next();
-}
-)
+})
+app.get("/user",(req,res,next)=>{
+    console.log("Entry level 4 and the fourth changes happen to the request object");
+    res.send("After performing all the changes on the request by above middlewares, we are sending the response to the client");
+    next();//it not gives an err while there is no any next middleware but it is not a good practice to use next() in the last middleware, because it will be confusing for the other developers who will read your code.
+})
 
 
 app.listen(7777,() => {
