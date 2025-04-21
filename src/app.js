265620a3b29e8c3937955ca7c459328fe069d426
Devarsh07 <debarshupadhyay7@gmail.com>
2025-04-22 03:49:36 +0530

    Database connection with Good Practice

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const app = express();
 const {adminAuth,userAuth}= require("./middlewares/auth");
-require("./config/database");
+const connectDB = require("./config/database");
+
+
+//since as we telling above that this function return promise so, we have to handled the value returned from promised function by then and catch
+connectDB()
+    .then(() =>{
+        console.log("Database connection Established");
+        app.listen(7777,() => {      //now firstly database is connected at line 4 and then server is running at this line.
+            console.log('Server is listening on port 7777')
+        });
+    })
+    .catch((err)=>{
+        console.log("MongoDB Connection ERror!");
+    });
+
 
 app.get("/user",(req,res,next)=>{
     try{
@@ -12,9 +26,4 @@ app.get("/user",(req,res,next)=>{
         console.log("Error is handled in the catch block!");
         res.status(500).send("Error is handled in the catch block!");
     }
-});
-
-
-app.listen(7777,() => {
-    console.log('Server is listening on port 7777')
 });
--- src/config/database.js
+++ src/config/database.js
@@ -9,13 +9,4 @@ const connectDB = async() =>{
     await mongoose.connect("mongodb+srv://devarshupadhyay7:O8CmY10I81bBdHL5@namastenode.ce1jfxd.mongodb.net/?retryWrites=true&w=majority&appName=NamasteNode");
 };
 
-
-//since as we telling above that this function return promise so, we have to handled the value returned from promised function by then and catch
-connectDB()
-    .then(() =>{
-        console.log("Database connection Established");
-    })
-    .catch((err)=>{
-        console.log("MongoDB Connection ERror!");
-    });
-
+module.exports = connectDB;
