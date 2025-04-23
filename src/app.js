1af36874d18571fd258ecbada2edb3ace5f2785d
Devarsh07 <debarshupadhyay7@gmail.com>
2025-04-23 10:42:42 +0530

    data added dynamically from req.body and using the express , json parsing middleware

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const {adminAuth,userAuth}= require("./middlewares/auth");
 const connectDB = require("./config/database");
 const User = require("./models/user");
 
+app.use(express.json())//parsing the json data to js object
+
 //since as we telling above that this function return promise so, we have to handled the value returned from promised function by then and catch
 connectDB()
     .then(() =>{
@@ -15,18 +17,11 @@ connectDB()
     .catch((err)=>{
         console.log("MongoDB Connection ERror!");
     });
-app.post("/signUp", async(req,res,next)=>{
-    const userObj = {
-        firstName : "lashkare",
-        lastName : "Taliban",
-        email : "lashkare@taliban.com",
-        password : "12345",
-        age : 21,
-        gender : "Not defined",
-        // _id : 12345675432345633565, always try to dont crerate an id lets take mogoDB create itself
-    }
+app.post("/signUp",async(req,res,next)=>{
     
-    const user = new User(userObj);//here the instance we have created named user is the new document with these fields.
+    //here req.body have a data in json format which was converted to js object by above middleware and it was pass by postman by post method
+    const user = new User(req.body);//here the instance we have created named user is the new document with these fields.
+
     try{
         await user.save();
         res.send("Database Updated Successfully");    
